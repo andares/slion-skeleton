582d266799d29df3d7f7643a6a9fee0d8323fd72
Andares Merigold <andares@outlook.com>
2016-07-14 11:50:48 +0800

    update for unittest phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,9 @@
 define('IN_TEST', 1);
 
 $GLOBALS['settings'] = require __DIR__ . '/../src/settings.php';
+// 改本地连接数据库
+$GLOBALS['settings']['slion_settings']['utils']['config'][1] = 'local';
+
 require __DIR__ . '/../vendor/autoload.php';
 $app = $GLOBALS['app'];
 /* @var $app \Slim\App */
